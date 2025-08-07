ba8bb705be1bc025f62ec8faf70c2fafb2f681d6
dcasarin-schumanassociates <davide.casarin@schumanassociates.com>
2025-08-07 12:00:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import re
 import pandas as pd
 from io import BytesIO
 
-st.set_page_config(page_title="Horizon Topic Extractor", layout="centered")
+st.set_page_config(page_title="Horizon Topic Organiser", layout="centered")
 st.title("📄 Horizon Topic Extractor")
 st.write("Upload a Horizon Europe PDF file and get an Excel sheet with parsed topics.")
 
